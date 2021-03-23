8d2324836b29636fab9bf5d8c48e447cc0cc6fc4
monreyes7 <81212258+monreyes7@users.noreply.github.com>
2021-03-23 02:32:41 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-
+import { valida } from "../js/main.js";
+export class Saludo {
+  /**@param {string} nombre*/
+  calculaSaludo(email){
+    const emailSinEspacios = email ? email.trim() : "";
+    valida(emailSinEspacios, "Falta proporcoanar el email.");
+    return "Hola $(emailSinEspacios}.;
+  }
+}
